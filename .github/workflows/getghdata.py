ae660d548ae7bd4b2ae6d292b5639753d7862d5e
Julien Yensen Martin <julien.yensen-martin@gologic.ca>
2024-11-27 15:13:24 -0500

    Add defectdojo reports test

---
+++ .github/workflows/getghdata.py
@@ -0,0 +1,106 @@
+def make_query(after_cursor=None):
+    return """
+query getVulnerabilitiesByRepoAndOwner($name: String!, $owner: String!) {
+  repository(name: $name, owner: $owner) {
+    vulnerabilityAlerts(first: 100, after:AFTER, states: OPEN) {
+      nodes {
+        id
+        createdAt
+        vulnerableManifestPath
+        securityVulnerability {
+          severity
+          updatedAt
+          package {
+            name
+            ecosystem
+          }
+          firstPatchedVersion {
+            identifier
+          }
+          vulnerableVersionRange
+          advisory {
+            description
+            summary
+            identifiers {
+              value
+              type
+            }
+            references {
+              url
+            }
+            cvss {
+              vectorString
+            }
+          }
+        }
+        vulnerableManifestPath
+        state
+        vulnerableManifestFilename
+        vulnerableRequirements
+        number
+        dependencyScope
+        dismissComment
+        dismissReason
+        dismissedAt
+        fixedAt
+      }
+      totalCount
+      pageInfo {
+        endCursor
+        hasNextPage
+        hasPreviousPage
+        startCursor
+      }
+    }
+    nameWithOwner
+    url
+  }
+}
+""".replace(
+        "AFTER", '"{}"'.format(after_cursor) if after_cursor else "null"
+    )
+
+# accumulates all pages data into a single object
+def get_dependabot_alerts_repository(repo, owner):
+    keep_fetching = True
+    after_cursor = None
+    output_result = {"data": {"repository": {"vulnerabilityAlerts": {"nodes": []}}}}
+    while keep_fetching:
+        headers = {"Authorization": AUTH_TOKEN}
+
+        request = requests.post(
+            url="https://api.github.com/graphql",
+            json={
+                "operationName": "getVulnerabilitiesByRepoAndOwner",
+                "query": make_query(after_cursor),
+                "variables": {"name": repo, "owner": owner},
+            },
+            headers=headers,
+        )
+
+        result = request.json()
+        output_result["data"]["repository"]["name"] = result["data"]["repository"][
+            "name"
+        ]
+        output_result["data"]["repository"]["url"] = result["data"]["repository"]["url"]
+        if result["data"]["repository"]["vulnerabilityAlerts"]["totalCount"] == 0:
+            return None
+
+        output_result["data"]["repository"]["vulnerabilityAlerts"]["nodes"] += result[
+            "data"
+        ]["repository"]["vulnerabilityAlerts"]["nodes"]
+
+        keep_fetching = result["data"]["repository"]["vulnerabilityAlerts"]["pageInfo"][
+            "hasNextPage"
+        ]
+        after_cursor = result["data"]["repository"]["vulnerabilityAlerts"]["pageInfo"][
+            "endCursor"
+        ]
+    print(
+        "Fetched {} alerts for repo {}/{}".format(
+            result["data"]["repository"]["vulnerabilityAlerts"]["totalCount"],
+            owner,
+            repo,
+        )
+    )
+    return json.dumps(output_result, indent=2)
---
+++ .github/workflows/upload.py
@@ -0,0 +1,38 @@
+import requests
+import sys
+import os
+
+file_name = sys.argv[1]
+scan_type = ''
+
+if file_name == 'gitleaks.json':
+    scan_type = 'Gitleaks Scan'
+elif file_name == 'njsscan.sarif':
+    scan_type = 'SARIF'
+elif file_name == 'semgrep.json':
+    scan_type = 'Semgrep JSON Report'
+
+apitoken = os.environ.get('DEFECTDOJO_API_TOKEN')
+headers = {
+    print("'Authorization': 'Token %s'" % (apitoken))
+}
+
+url = os.environ.get('DEFECTDOJO_IMPORT_API_URL')
+data = {
+    'active': True,
+    'verified': True,
+    'scan_type': scan_type,
+    'minimum_severity': 'Low',
+    'engagement': 19
+}
+
+files = {
+    'file': open(file_name, 'rb')
+}
+
+response = requests.post(url, headers=headers, data=data, files=files)
+
+if response.status_code == 201:
+    print('Scan results imported successfully')
+else:
+    print(f'Failed to import scan results: {response.content}')
